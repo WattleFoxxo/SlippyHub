4cf386765db523bc9a9a495798af9abd51743db5
WattleFoxxo <88825700+WattleFoxxo@users.noreply.github.com>
2023-09-09 02:05:30 +1000

    Added base64 messages

--- server/modules/api.js
+++ server/modules/api.js
@@ -8,10 +8,10 @@ module.exports = function(config, app, io, port, parser) {
         var currentTime = new Date();
         var timestamp = `${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}.${currentTime.getMilliseconds()}`
         io.emit('serialRecive', data, timestamp);
-        var command = data.split("=");
-        if (command[0] == "AT+MESSAGE") { // Example message: 111.0.20.6,55.16.16.21,22341,1,AT+PING,-111,-5.7500000000
-            var param = command[1].split(",");
-            io.emit('slippyRecive', param[0], param[1], param[2], param[3], param[4], timestamp);
+
+        if (data.startsWith("AT+MESSAGE=")) { // Example message: 111.0.20.6,55.16.16.21,22341,1,YWxleGQgbW9tZW50ICg2Myk=,-111,-5.7500000000
+            var param = data.replace("AT+MESSAGE=", "").split(",");
+            io.emit('slippyRecive', param[0], param[1], param[2], param[3], atob(param[4]), timestamp);
         }
 
         if (data.startsWith("Ready, Your address is: ")) {
@@ -26,12 +26,14 @@ module.exports = function(config, app, io, port, parser) {
 
         socket.on('serialReset', () => {
             port.close();
-            port.open();
+            setTimeout(function() {
+                port.open();
+            }, 1000);
         });
     
         socket.on('slippySend', (address, data) => {
             var addr = address.split(".");
-            port.write(`AT+SEND=${addr[0]},${addr[1]},${addr[2]},${addr[3]},${data}\r\n`);
+            port.write(`AT+SENDBASE64=${addr[0]},${addr[1]},${addr[2]},${addr[3]},${btoa(data)}\r\n`);
         });
 
         io.emit('slippyInfo', localAddress);
@@ -39,6 +41,8 @@ module.exports = function(config, app, io, port, parser) {
 
     setTimeout(function() {
         port.close();
-        port.open();
+        setTimeout(function() {
+            port.open();
+        }, 1000);
     }, 1000);
 };
